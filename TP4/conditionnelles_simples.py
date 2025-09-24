421d71181f1c33b9f300cfa17fe85fd91871af93
DJEGNON Yves <yves.djegnon.etu@univ-lille.fr>
2025-09-24 23:43:44 +0200

    j'essaie de commit

--- TP4/conditionnelles_simples.py
+++ TP4/conditionnelles_simples.py
@@ -16,11 +16,11 @@ def maximum_sem4(x:int,y:int)-> int:
 
     Précondition : aucune
     Exemple(s) :
-    $$$ maximum_sem4(2,7)
+    >>> maximum_sem4(2,7)
     7
-    $$$ maximum_sem4(3,1)
+    >>> maximum_sem4(3,1)
     3
-    $$$ maximum_sem4(3,3)
+    >>> maximum_sem4(3,3)
     3
     """
     
@@ -37,11 +37,11 @@ est strictement inférieur à y, 1 si x est strictement supérieur à y et 0 si 
 
     Précondition :
     Exemple(s) :
-    $$$ compare_sem4(1,1)
+    >>> compare_sem4(1,1)
     0
-    $$$ compare_sem4(1,2)
+    >>> compare_sem4(1,2)
     -1
-    $$$ compare_sem4(2,1)
+    >>> compare_sem4(2,1)
     1
     """
     if x<y:
@@ -58,11 +58,11 @@ def minimum3_sem4(x:int,y:int,z:int)->int:
 
     Précondition :aucune
     Exemple(s) :
-    $$$ minimum3_sem4(1,2,3)
+    >>> minimum3_sem4(1,2,3)
     1
-    $$$ minimum3_sem4(6,0,3)
+    >>> minimum3_sem4(6,0,3)
     0
-    $$$ minimum3_sem4(5,5,8)
+    >>> minimum3_sem4(5,5,8)
     5
     """
     if x<=y and x<=z:
@@ -79,24 +79,24 @@ AJOURNE='AJ'
 ENJAMB='AJAC'
 
 def resultat_annee(BCC1_S1:bool, BCC2_S1:bool, BCC1_S2:bool, BCC2_S2:bool)->str:
-    """ renvoie une chaîne qui peut être :
+    """Renvoie une chaîne qui peut être :
     • "ADM" dans le cas où les 4 BCC sont validés ou, à défaut,
-    • "AJAC" dans le cas où les BCC1 des 2 semestres ont été validés, ou , à défaut,
+    • "AJAC" dans le cas où les BCC1 des 2 semestres ont été validés, ou, à défaut,
     • une chaîne qui commence par "AJ", suivi d’un texte qui indique que les notes des BCC validés peuvent être
-    conservées.
+      conservées.
 
     Précondition : aucune
     Exemple(s) :
-    $$$ resultat_annee(True, True, True, True)
-    "ADM"
-    $$$ resultat_annee(True, False, True, True)
-    "AJAC"
-    $$$ resultat_annee(False, False, False, False)
-    "AJ"
-    $$$ resultat_annee(True, False, False, False)
-    "AJ\\nVous gardez votre note du BCC1 du S1"
-    $$$ resultat_annee(True, False, False, True)
-    ""'AJ\nVous gardez votre note du BCC1 du S1\nVous gardez votre note du BCC2 du S1'
+    >>> resultat_annee(True, True, True, True)
+    'ADM'
+    >>> resultat_annee(True, False, True, True)
+    'AJAC'
+    >>> resultat_annee(False, False, False, False)
+    'AJ'
+    >>> resultat_annee(True, False, False, False)
+    'AJ\\nVous gardez votre note du BCC1 du S1'
+    >>> resultat_annee(True, False, False, True)
+    'AJ\\nVous gardez votre note du BCC1 du S1\\nVous gardez votre note du BCC2 du S1'
     """
     texte= "\nVous gardez votre note du BCC"
 
@@ -118,4 +118,7 @@ def resultat_annee(BCC1_S1:bool, BCC2_S1:bool, BCC1_S2:bool, BCC2_S2:bool)->str:
         
         
     
-        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    print("✅ Tests doctest exécutés")        
