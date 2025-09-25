4fcfac7188350239db5511c9b7bea8f102eda9f2
DJEGNON Yves <yves.djegnon.etu@univ-lille.fr>
2025-09-25 04:53:37 +0200

    2ème fichier

---
+++ TP4/conditionnelles_synthese.py
@@ -0,0 +1,46 @@
+# Compléter ici (noms, groupe, contenu fichier, date)
+# DJEGNON Yves
+# MI 12
+# TP4
+# 25/09/2025
+# Ne pas supprimer cette ligne. <trace>conditionnelles_synthese.py</trace>
+
+####################
+# Exercices de synthèse
+####################
+
+####################
+# Location
+####################
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def cout_location(nb_jours, nb_km, energie
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def arg_minimum
+
+# Décommenter et compléter la signature donnée puis faire la suite
+def conseil_voiture(nb_jours, nb_km):
+    """"
+    
+        Précondition :
+        Exemple(s) :
+        >>>
+""""   
+    
+
+####################
+# Impression à tarif dégressif
+####################
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def nombre_exemplaires
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def montant_facture
+
+####################
+# Roulette
+####################
+
+#def calcul_gain(bille, pari, mise
