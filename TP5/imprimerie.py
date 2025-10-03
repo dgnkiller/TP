9e3d4a727cb9926a270afbd586ebc00e81d20ce4
Djegnon Yves <yves.djegnon.etu@120p5.fil.univ-lille.fr>
2025-10-03 11:42:07 +0200

    ajout tp5

---
+++ TP5/imprimerie.py
@@ -0,0 +1,8 @@
+# Compléter ici (noms, groupe, contenu fichier, date)
+#
+#
+#
+#
+# Ne pas supprimer cette ligne. <trace>imprimerie.py</trace>
+
+# Vous êtes libres d'utiliser les noms de fonction de votre choix. 
---
+++ TP5/jeu_421.py
@@ -0,0 +1,36 @@
+# Compléter ici (noms, groupe, contenu fichier, date)
+#
+#
+#
+#
+# Ne pas supprimer cette ligne. <trace>jeu_421.py</trace>
+
+####### fonctions d'affichage
+
+#def affiche_congratulation(
+
+#def affiche_perdant(
+
+#def affiche_presentation_jeu(
+
+#def affiche_lancer(
+
+####### fonctions de saisie
+
+#def saisie_continuation(
+
+#def def saisie_pseudo(
+
+####### fonctions de calcul
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def lancer_de(
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def est_421(
+
+# Décommenter et compléter la signature donnée puis faire la suite
+#def veut_continuer(
+
+###### programme principal
+
